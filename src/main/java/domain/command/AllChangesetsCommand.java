ca1225acbc0555077a8f86588782042abf36dea7
Lombrozo <volodya.lombrozo@yandex.ru>
2020-12-17 18:24:59 +0300

    AllChangesetsCommand was added (hg log --debug --template "user:'{author}'\nbranch:'{branch}'\ndate:'{date|isodate}'\nmessage:'{desc}'\nnode:'{node}'\nrev:'{rev}'\ntags:'{tags}'\nparents:'{parents}'\n\n")

---
+++ src/main/java/domain/command/AllChangesetsCommand.java
@@ -0,0 +1,18 @@
+package domain.command;
+
+public class AllChangesetsCommand implements Command {
+    private final Command delegate;
+
+    public AllChangesetsCommand(String repoPath) {
+        this(new HgCommand(repoPath, "hg", "log", "--debug", "--template", "user:'{author}'\nbranch:'{branch}'\ndate:'{date|isodate}'\nmessage:'{desc}'\nnode:'{node}'\nrev:'{rev}'\ntags:'{tags}'\nparents:'{parents}'\n\n"));
+    }
+
+    public AllChangesetsCommand(Command delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public String execute() throws ExecuteException {
+        return delegate.execute();
+    }
+}
